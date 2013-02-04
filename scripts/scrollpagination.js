451cb11c5f1123ea5e753ab3efa839c766d62e5e
Lucas Pirola <lucaspirola@gmail.com>
2013-02-04 21:50:09 -0200

    code formatter

--- scripts/scrollpagination.js
+++ scripts/scrollpagination.js
@@ -1,134 +1,119 @@
 /*
-**	Anderson Ferminiano
-**	contato@andersonferminiano.com -- feel free to contact me for bugs or new implementations.
-**	jQuery ScrollPagination
-**	28th/March/2011
-**	http://andersonferminiano.com/jqueryscrollpagination/
-**	You may use this script for free, but keep my credits.
-**	Thank you.
-*/
+ **	Anderson Ferminiano
+ **	contato@andersonferminiano.com -- feel free to contact me for bugs or new implementations.
+ **	jQuery ScrollPagination
+ **	28th/March/2011
+ **	http://andersonferminiano.com/jqueryscrollpagination/
+ **	You may use this script for free, but keep my credits.
+ **	Thank you.
+ */
 
-(function( $ ){
-	 
-		 
- $.fn.scrollPagination = function(options) {
-  	
-		var opts = $.extend($.fn.scrollPagination.defaults, options);  
+(function($) {
+
+
+	$.fn.scrollPagination = function(options) {
+
+		var opts = $.extend($.fn.scrollPagination.defaults, options);
 		var target = opts.scrollTarget;
-		if (target == null){
-			target = obj; 
-	 	}
+		if(target == null) {
+			target = obj;
+		}
 		opts.scrollTarget = target;
-	 
-		return this.each(function() { 
-		  $.fn.scrollPagination.init($(this), opts);
+
+		return this.each(function() {
+			$.fn.scrollPagination.init($(this), opts);
 		});
 
-  };
-  
-  $.fn.stopScrollPagination = function(){
-	  return this.each(function() {
-	 	$(this).attr('scrollPagination', 'disabled');
-	  });
-	  
-  };
+	};
 
-  // Simple JavaScript Templating
-  // John Resig - http://ejohn.org/ - MIT Licensed
-  $.fn.scrollPagination.tmpl = function tmpl(str, data){
-  	var cache = {};
-    // Figure out if we're getting a template, or if we need to
-    // load the template - and be sure to cache the result.
-    var fn = !/\W/.test(str) ?
-      cache[str] = cache[str] ||
-        tmpl(document.getElementById(str).innerHTML) :
-      
-      // Generate a reusable function that will serve as a template
-      // generator (and which will be cached).
-      new Function("obj",
-        "var p=[],print=function(){p.push.apply(p,arguments);};" +
-        
-        // Introduce the data as local variables using with(){}
-        "with(obj){p.push('" +
-        
-        // Convert the template into pure JavaScript
-        str
-          .replace(/[\r\t\n]/g, " ")
-          .split("<%").join("\t")
-          .replace(/((^|%>)[^\t]*)'/g, "$1\r")
-          .replace(/\t=(.*?)%>/g, "',$1,'")
-          .split("\t").join("');")
-          .split("%>").join("p.push('")
-          .split("\r").join("\\'")
-      + "');}return p.join('');");
-    
-    // Provide some basic currying to the user
-    return data ? fn( data ) : fn;
-  }; 
+	$.fn.stopScrollPagination = function() {
+		return this.each(function() {
+			$(this).attr('scrollPagination', 'disabled');
+		});
 
-  
-  $.fn.scrollPagination.loadContent = function(obj, opts){
-	 var target = opts.scrollTarget;
-	 var mayLoadContent = $(target).scrollTop()+opts.heightOffset >= $(document).height() - $(target).height();
-	 if (mayLoadContent){
-		 if (opts.beforeLoad != null){
-			opts.beforeLoad(); 
-		 }
-		 $(obj).children().attr('rel', 'loaded');
-		 $.ajax({
-			  type: 'POST',
-			  url: opts.contentPage,
-			  data: opts.contentData,
-			  success: function(data){
-			  	if (opts.dataType == 'json') {
-			  		$.each(data, function (key, value) {
-			  			$(obj).append($.fn.scrollPagination.tmpl(opts.template, value)); 
-			  			var objectsRendered = $(obj).children('[rel!=loaded]');
-					
-						if (opts.afterLoad != null){
-							opts.afterLoad(objectsRendered);	
-						}
-			  		});
-			  	} else {
-			  		$(obj).append(data); 
+	};
+
+	// Simple JavaScript Templating
+	// John Resig - http://ejohn.org/ - MIT Licensed
+	$.fn.scrollPagination.tmpl = function tmpl(str, data) {
+		var cache = {};
+		// Figure out if we're getting a template, or if we need to
+		// load the template - and be sure to cache the result.
+		var fn = !/\W/.test(str) ? cache[str] = cache[str] || tmpl(document.getElementById(str).innerHTML) :
+
+		// Generate a reusable function that will serve as a template
+		// generator (and which will be cached).
+		new Function("obj", "var p=[],print=function(){p.push.apply(p,arguments);};" +
+
+		// Introduce the data as local variables using with(){}
+		"with(obj){p.push('" +
+
+		// Convert the template into pure JavaScript
+		str.replace(/[\r\t\n]/g, " ").split("<%").join("\t").replace(/((^|%>)[^\t]*)'/g, "$1\r").replace(/\t=(.*?)%>/g, "',$1,'").split("\t").join("');").split("%>").join("p.push('").split("\r").join("\\'") + "');}return p.join('');");
+
+		// Provide some basic currying to the user
+		return data ? fn(data) : fn;
+	};
+
+
+	$.fn.scrollPagination.loadContent = function(obj, opts) {
+		var target = opts.scrollTarget;
+		var mayLoadContent = $(target).scrollTop() + opts.heightOffset >= $(document).height() - $(target).height();
+		if(mayLoadContent) {
+			if(opts.beforeLoad != null) {
+				opts.beforeLoad();
+			}
+			$(obj).children().attr('rel', 'loaded');
+			$.ajax({
+				type: 'POST',
+				url: opts.contentPage,
+				data: opts.contentData,
+				success: function(data) {
+
+					if(opts.dataType == 'json') {
+						$.each(data, function(key, value) {
+							$(obj).append($.fn.scrollPagination.tmpl(opts.template, value));
+						});
+					} else {
+						$(obj).append(data);
+					}
+
 					var objectsRendered = $(obj).children('[rel!=loaded]');
-					
-					if (opts.afterLoad != null){
-						opts.afterLoad(objectsRendered);	
-					}	
-			  	}
-			  },
-			  dataType: opts.dataType
-		 });
-	 }
-	 
-  };
-  
-  $.fn.scrollPagination.init = function(obj, opts){
-	 var target = opts.scrollTarget;
-	 $(obj).attr('scrollPagination', 'enabled');
-	
-	 $(target).scroll(function(event){
-		if ($(obj).attr('scrollPagination') == 'enabled'){
-	 		$.fn.scrollPagination.loadContent(obj, opts);		
+
+					if(opts.afterLoad != null) {
+						opts.afterLoad(objectsRendered);
+					}
+				},
+				dataType: opts.dataType
+			});
 		}
-		else {
-			event.stopPropagation();	
-		}
-	 });
-	 
-	 $.fn.scrollPagination.loadContent(obj, opts);
-	 
- };
-	
- $.fn.scrollPagination.defaults = {
-      	 'contentPage' : null,
-     	 'contentData' : {},
-		 'beforeLoad': null,
-		 'afterLoad': null	,
-		 'scrollTarget': null,
-		 'heightOffset': 0,
-		 'dataType': 'html',
-		 'template': '<li style="opacity:0;-moz-opacity: 0;filter: alpha(opacity=0);"><p><%= title %></p></li>'
- };	
-})( jQuery );
+
+	};
+
+	$.fn.scrollPagination.init = function(obj, opts) {
+		var target = opts.scrollTarget;
+		$(obj).attr('scrollPagination', 'enabled');
+
+		$(target).scroll(function(event) {
+			if($(obj).attr('scrollPagination') == 'enabled') {
+				$.fn.scrollPagination.loadContent(obj, opts);
+			} else {
+				event.stopPropagation();
+			}
+		});
+
+		$.fn.scrollPagination.loadContent(obj, opts);
+
+	};
+
+	$.fn.scrollPagination.defaults = {
+		'contentPage': null,
+		'contentData': {},
+		'beforeLoad': null,
+		'afterLoad': null,
+		'scrollTarget': null,
+		'heightOffset': 0,
+		'dataType': 'html',
+		'template': '<li style="opacity:0;-moz-opacity: 0;filter: alpha(opacity=0);"><p><%= title %></p></li>'
+	};
+})(jQuery);
